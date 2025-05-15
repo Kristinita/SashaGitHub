ff383e7ca8a1f9d4f2d57ab585601c9ab14bfe9b
Kristinita <Kristinita@users.noreply.github.com>
2025-05-15 08:30:40 +0300

    test(changelogen): try default type not defined in the file changelog.config.mjs
    Signed-off-by: Kristinita <Kristinita@users.noreply.github.com>

--- changelog.config.mjs
+++ changelog.config.mjs
@@ -2,8 +2,9 @@
 	@Author: SashaChernykh
 	@Date: 2025-05-15 08:22:30
 	@Last Modified by: SashaChernykh
-	@Last Modified time: 2025-05-15 08:23:48
+	@Last Modified time: 2025-05-15 08:29:38
 */
+
 export default {
 
 	"types": {
